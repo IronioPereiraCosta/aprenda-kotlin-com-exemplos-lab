94eb706517641740c64a0b2abb4b114a8a099f2b
Ironio <onepereira2810@gmail.com>
2022-12-09 14:02:58 -0300

    ajuste nos valores do enu nivel

--- desafio.kt
+++ desafio.kt
@@ -19,3 +19,33 @@ fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+
+
+
+ enum class nivel {BASICO,INTERMEDIARIO,FASIL}
+
+ class Usuario
+
+data class user(val nome: String, val ind: Int = 50)
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+
+    fun main() {
+    val user = user("Peu", 1)
+    pritln(user)
+    
+    val secundUser = User("Peu", 1)
+    val secundUser = User("Piu", 2)
+    
+   fun maftricular(usuario: Usuario) {
+       
+      
+   
+}
+
+fun main() {
+    val start = start.RUNNING
+    val mensagem = whens (state ) {
+        start.RUNNING->"It' s running"
+    }
+    pritln(mensage) 
